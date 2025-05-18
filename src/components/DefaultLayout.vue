391a8acddf0472d502693c3cd13a17bfd331964b
Joey A. Cabelin <joey.cabelin.1@gmail.com>
2025-05-18 23:44:50 +0800

    Somechanges

---
+++ src/components/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+import PageHeader from './PageHeader.vue';
+import PageFooter from './PageFooter.vue';
+
+export default {
+    components: {
+        PageHeader,
+        PageFooter,
+    }
+}
+
+</script>
