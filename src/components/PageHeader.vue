18779524f354eb91da39851648fbd7bec20b3b13
Joey A. Cabelin <joey.cabelin.1@gmail.com>
2025-06-02 20:03:22 +0800

    Somechanges

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,3 +1,9 @@
 <template>
+    <PageLogo/>
     <h1>Header </h1>
 </template>
+
+<script setup>
+import PageLogo from './composables/PageLogo.vue';
+
+</script>
---
+++ src/components/composables/PageLogo.vue
@@ -0,0 +1,8 @@
+<template>
+    <router-link to="">
+        <h1 class="uppercase text-xl font-bold">
+            <span>Car</span>
+            <span>Book</span>
+        </h1>
+    </router-link>
+</template>
