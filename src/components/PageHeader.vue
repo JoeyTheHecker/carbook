fae12fa326f7a8c00e6d86bd48504055b7e3a910
Joey A. Cabelin <joey.cabelin.1@gmail.com>
2025-06-05 19:45:36 +0800

    Some changes

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,6 +1,8 @@
 <template>
-    <PageLogo/>
-        <nav>
+    <header class="flex justify-center px-6 p-2 bg-amber-500">
+        <div class="flex justify-between item-center w-full md:w-5/6 md:relative z-50">
+        <PageLogo/>
+        <nav class="space-x-6 bg-amber-300">
             <router-link 
                 v-for="link in links"
                 :key="link.path"
@@ -9,12 +11,14 @@
                 >{{ link.name }}
             </router-link>
         </nav>
+    </div>
+    </header>
 </template>
 
 <script setup>
 import PageLogo from './composables/PageLogo.vue';
 import { useRoute } from 'vue-router';
-
+//33:23
 const route = useRoute();
 
 const links = [
